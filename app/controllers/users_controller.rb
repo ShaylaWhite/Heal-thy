6058a054d30de263bbc0eab5983a97c53c3fa2a9
shayla_white <shayladwhite1@gmail.com>
2020-05-05 19:46:56 -0700

    create post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController 
 
+    #shows login page /login from| 
+
     get '/login' do
         erb :login
     end
-   #shows login page /login from
+
+    #post the user login information 
+    #create a sessin log user in and after user is found 
+    post '/login' do 
+       
+    end
+ 
 
 
 
