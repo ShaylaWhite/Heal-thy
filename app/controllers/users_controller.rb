e360d910eb0ebc7e830dd4a254b36a30594193c6
shayla_white <shayladwhite1@gmail.com>
2020-05-05 19:42:31 -0700

    login/signup route and mount the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,13 @@
-class UsersControllers:: ApplicationController 
+class UsersController < ApplicationController 
+
+    get '/login' do
+        erb :login
+    end
+   #shows login page /login from
+
+
+
+    get '/signup' do
+    end
 
 end 
